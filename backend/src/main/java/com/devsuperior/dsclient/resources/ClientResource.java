9a08498a969e245b079c33c461ebf9d3af8e5bad
stryder <paulim@gmail.com>
2022-07-30 13:28:59 -0300

    Exceptions client

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.services.ClientService;
 
 
@@ -21,10 +22,10 @@ import com.devsuperior.dsclient.services.ClientService;
 		private ClientService service;
 		
 		
-		@GetMapping
-		public ResponseEntity<List<Client>> findAll() {
-			List<Client> list = service.findAll();		
-			return ResponseEntity.ok().body(list);
+		@GetMapping(value = "/{id}")
+		public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {			
+			ClientDTO dto = service.findById(id);		
+			return ResponseEntity.ok().body(dto);
 			
 		}
 	
--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -1,12 +1,17 @@
 package com.devsuperior.dsclient.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,9 +20,18 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll() {
-		return repository.findAll();
+	@Transactional (readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
 		
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());		
+	}
+	
+	@Transactional (readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));		
+		return new ClientDTO(entity);
 	}
 	
 
