fd17a60adc66b91ba1b70eced17382effd144ee8
stryder <paulim@gmail.com>
2022-07-30 15:12:47 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/dsclient/entities/Client.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 
@@ -22,11 +24,15 @@ public class Client implements Serializable {
 	private Long id;
 	private String name;
 	private String cpf;
-	private Double income;
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Double income;	
 	private Instant birthDate;
 	private Integer children;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	public Client() {		
 	}
@@ -99,6 +105,26 @@ public class Client implements Serializable {
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}	
+
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();		
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();		
 	}
 
 
--- backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -1,18 +1,22 @@
 package com.devsuperior.dsclient.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsclient.dto.ClientDTO;
 import com.devsuperior.dsclient.entities.Client;
 import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
 import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -23,10 +27,10 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional (readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
 		
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());		
+		return list.map(x -> new ClientDTO(x));		
 	}
 	
 	@Transactional (readOnly = true)
@@ -67,6 +71,20 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id Not Found" + id);
 		}
 		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}	
+		
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);			
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+			
+		}
 	}
 		
 
