356ca8af07f00d90abb1925f9f5eaf6b4fe10335
stryder <paulim@gmail.com>
2022-07-30 14:13:39 -0300

    Update Client

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +38,13 @@ import com.devsuperior.dsclient.services.ClientService;
 			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 					.buildAndExpand(dto.getId()).toUri();
 			return ResponseEntity.created(uri).body(dto);
+			
+		}
+		
+		@PutMapping(value = "/{id}")
+		public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+			dto = service.update(id, dto);			
+			return ResponseEntity.ok().body(dto);
 			
 		}
 	
